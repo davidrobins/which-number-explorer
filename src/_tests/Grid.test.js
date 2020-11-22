21adf39c78ed947b87e59c308d5de84f64d07c19
davidrobins <david@dlrobins.co.uk>
2020-11-22 10:12:03 +0000

    test setup

---
+++ src/_tests/Grid.test.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Grid from '../Grid'
+
+configure({ adapter: new Adapter() });
+
+describe('Grid', () => {
+    const defaultProps = {
+        ceiling: 144
+    }
+    let component;
+    beforeEach(() => {
+        component = shallow(<Grid {...defaultProps} />)
+    })
+    it('exists', () => {
+        expect(component.exists()).toBeTruthy()
+    });
+})
